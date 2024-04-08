d960b8f2f0a132155e423f1026b501b87ed49491
Zina <markosyan.zina23@gmail.com>
2024-04-08 12:00:07 +0200

    add scss

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,10 @@
 import useAppData from "../../hooks/useAppData"
+import './index.scss'
 
 const HomePage = () => {
   const { count } = useAppData()
   console.log({count})
-  return <div>
+  return <div className="container">
     This is HomePage
     <p>{count}</p>
   </div>
